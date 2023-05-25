074c7f3055ff362000851327fbdae4615f7dc68d
Trung Hieu <trunghieusn2002@gmail.com>
2023-05-25 19:29:47 +0700

    Add origin cross to all controller

--- src/main/java/com/raovat/api/image/ImageController.java
+++ src/main/java/com/raovat/api/image/ImageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/image")
 @SecurityRequirement(name = "Bearer Authentication")
--- src/main/java/com/raovat/api/post/PostController.java
+++ src/main/java/com/raovat/api/post/PostController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/post")
 @RequiredArgsConstructor
