a6dcecc4bde9f9df3dd74c3a1e2f1664275d38fb
Trung Hieu <trunghieusn2002@gmail.com>
2023-05-25 18:08:32 +0700

    Enable cros

--- src/main/java/com/raovat/api/config/SecurityConfiguration.java
+++ src/main/java/com/raovat/api/config/SecurityConfiguration.java
@@ -26,6 +26,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
