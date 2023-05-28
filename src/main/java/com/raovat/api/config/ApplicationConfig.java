4cacccec838953611dd691b6947b4909d8d49059
Trung Hieu <trunghieusn2002@gmail.com>
2023-05-28 12:45:38 +0700

    Add allowedHeaders

--- src/main/java/com/raovat/api/config/ApplicationConfig.java
+++ src/main/java/com/raovat/api/config/ApplicationConfig.java
@@ -87,6 +87,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedHeaders("*");
     }
 }
