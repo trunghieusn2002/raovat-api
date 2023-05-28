456d6927c44ddbe3f63863e0dccec056ff85f7c7
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-05-28 14:00:07 +0700

    fix cros

--- src/main/java/com/raovat/api/config/SecurityConfiguration.java
+++ src/main/java/com/raovat/api/config/SecurityConfiguration.java
@@ -30,8 +30,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
