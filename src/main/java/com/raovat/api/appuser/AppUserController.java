209ebe2414b1ca0aa97c0c7c71bf624b84c9175a
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-05-28 13:32:21 +0700

    fix lỗi cros

--- src/main/java/com/raovat/api/appuser/AppUserController.java
+++ src/main/java/com/raovat/api/appuser/AppUserController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
 @RequestMapping("api/v1/app-user")
 @RequiredArgsConstructor
