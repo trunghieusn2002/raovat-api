22e685b93eabd852745ae1227126c8188e39ced2
Trung Hieu <trunghieusn2002@gmail.com>
2023-05-30 21:17:30 +0700

    Update role

--- src/main/java/com/raovat/api/appuser/dto/AppUserDTO.java
+++ src/main/java/com/raovat/api/appuser/dto/AppUserDTO.java
@@ -1,6 +1,7 @@
 package com.raovat.api.appuser.dto;
 
+import com.raovat.api.appuser.AppUserRole;
 import com.raovat.api.image.dto.ImageDTO;
 
-public record AppUserDTO(String firstName, String lastName, String email, String phone) {
+public record AppUserDTO(String firstName, String lastName, String email, String phone, AppUserRole appUserRole) {
 }
